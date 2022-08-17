b13d2789a8f5025acaf37e475236d45962f9d50e
denismoldovan-v <iamdenismoldovan@gmail.com>
2022-08-17 18:28:44 +0200

    commit before changes

---
+++ password_generator/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import random
+
+def home(request):
+	return render(request, "home.html")
+
+def about(request):
+	return render(request, 'about.html')
+
+def password(request):
+	password = ''
+	length = int(request.GET.get('select'))
+	letters = list('abcdefghijklmnopqrstuvwxyz')
+	if request.GET.get('symbols') == 'on':
+		letters.extend(list('!@#$%^&*()_+-='))
+	if request.GET.get('numbers') == 'on':
+		letters.extend(list('1234567890'))
+	if request.GET.get('uppercase') == 'on':
+		letters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+	for i in range(length):
+		password += random.choice(letters)
+	return render(request, "password.html", {'password' : password})
